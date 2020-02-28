c83b38a1a7d4ac62e995fa324bc08fa8666f3507
RAMRV <60852235+RAMRV@users.noreply.github.com>
2020-02-28 18:24:10 +0530

    Update dice_roller.py

--- dice_roller.py
+++ dice_roller.py
@@ -1,14 +1,23 @@
 
 def main():
   import random
-  dice_rolls = 2;
-  dice_sum = 0
+  
+  dice_rolls = int(input('How many dice to roll?')
+                   );
+  dice_size = int(input('How sides are of the dice?')
+                  );
+  dice_sum = 0;
   
   for i in range(0, dice_rolls):
-    roll = random.randint(1,6)
+    roll = random.randint(1,dice_size)
     #roll1 = random.randint(1,6);
-    dice_sum += roll
-    print(f'You rolled a  {roll}')
+    dice_sum += roll;
+    if roll == 1:
+      print(f'you rolled a {roll}, thats a 1')
+    elif roll ==6:
+      print(f'you rolled a {roll}, thats a 6')
+    else:
+      print(f'You rolled a  {roll}')
     
   print(f'You rolled a total of {dice_sum}')
   
